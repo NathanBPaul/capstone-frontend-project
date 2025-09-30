e95372b543e576c659b8ee8fa98874a73681c506
abhishektiwari-maker <abhishek.tiwari@kalvium.community>
2025-09-30 15:23:44 +0530

    Db db.json created

--- src/components/AddExpensePage.jsx
+++ src/components/AddExpensePage.jsx
@@ -1,5 +1,6 @@
 import React, { useState } from 'react'
 import '../styles/AddExpense.css'
+import axios from 'axios';
 const AddExpensePage = () => {
     const[expense,setExpense]=useState({
         title:"",
@@ -11,13 +12,28 @@ const AddExpensePage = () => {
         const name = e.target.name;
         setExpense({...expense,[name]:value});
     }
+    async function submitExpense(e){
+        e.preventDefault();
+        try {
+           await axios.post("http://localhost:5000/expenses",{
+            ...expense
+           }) 
+           console.log("Expense added successfully",expense);
+
+        } catch (error) {
+          alert("Error adding expense")  
+        }
+
+    }
   return (
     <form action=""
     id='add-expense'
+    onSubmit={submitExpense}
     >
         <input placeholder='Enter Title' type="text" name='title' onChange={handleChange} />
         <input placeholder='Enter Amount' type="Number"  name='amount' onChange={handleChange}/>
         <input placeholder='select Date' type="date" name='date' onChange={handleChange}/>
+        <input type="submit" value={"Add Expense"} />
     </form>
   )
 }
--- src/components/ExpenseList.jsx
+++ src/components/ExpenseList.jsx
@@ -2,17 +2,30 @@ import React, { useEffect,useState } from 'react'
 import Data from './Data'
 import ExpenseItem from './ExpenseItem'
 import ExpenseTotal from './ExpenseTotal';
-
+import axios from 'axios';
 const ExpenseList = () => {
     const[total,setTotal]=useState(0);
+    const [expences,setExpences]=useState([]);
 
+        async function fetchExpences(){
+            try {
+                const data = await axios.get("http://localhost:5000/expenses");
+                setExpences(data.data);
+                console.log(data);
+            } catch (error) {
+                console.log(error)
+            }
+        }
         useEffect(()=>{
-        let sum = 0;
-        Data().forEach((item)=>{
+        fetchExpences();       
+         },[])
+         useEffect(()=>{
+            let sum= 0;
+            expences.forEach((item)=>{
             sum += item.amount;
         })
         setTotal(sum);
-    },[])
+         },[expences])
     return (
         <>
     <table>
@@ -22,7 +35,7 @@ const ExpenseList = () => {
             <th>Date</th>
         </tr>
         {
-            Data().map((item)=>{
+            expences.map((item)=>{
                 return(
                     <ExpenseItem amount={item.amount} date={item.date} title={item.title}/>
                 )
